68775348f1dc3e91192489b38ba3d60236344a5c
satyasree2236 <104473988+satyasree2236@users.noreply.github.com>
2022-04-27 11:09:55 -0500

    Add files via upload

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,14 @@
+package JavaPackage;
+
+public class SumOfTwoNumbers {
+	
+	public static void main(String[] args) {
+		
+		int a= 10;
+		int b= 20;
+		int c= a+b;
+		
+		System.out.println(c);
+	}
+
+}
